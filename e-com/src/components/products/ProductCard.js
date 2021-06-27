731c0a76aa9fd057829f195de65fc931487dea23
Alexander Markianose <markianos.alexander@gmail.com>
2021-06-28 01:40:33 +0200

    product detail + detail props

--- e-com/src/components/products/ProductCard.js
+++ e-com/src/components/products/ProductCard.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-const ProductCard = ({product}) => {
+const ProductCard = ({product, details}) => {
     return (
         <div className="col">
             <div className="card h-100">
-                <img src={product.image} alt="..." className="card-img-top" />
+                {details && <h1 className="text-center">{product.name}</h1>}
+                <img src={product.image} alt="..." className="card-img-top h-100 p-5" />
                 <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
+                    {!details && <h5 className="card-title">{product.name}</h5>}
                     <div className="card-text">
-                        <p>{product.short}</p>
-                        <p className="text-danger text-right h5">{product.price} SEK</p>
+                       {
+                       details 
+                        ? <p>{product.desc}</p>
+                        : <p>{product.short}</p> 
+                       }
+                       <p className="text-danger text-right h5">{product.price} SEK</p>
                     </div>
                     <div className="d-flex justify-content-between mt-5">
                         <button className="btn btn-info">Add to Cart</button>
+                    {
+                        !details && 
                         <Link className="btn btn-info" to={`/products/${product._id}`}>read more</Link>
+                    }
                     </div>
+                    {
+                        details && 
+                        <div>
+                            <Link className="btn btn-dark mt-5" to="/products">Go back</Link>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
-    )
+    ) 
+}
+
+ProductCard.defaultProps = {
+    details: false 
 }
 
 export default ProductCard
--- e-com/src/views/ProductDetails.js
+++ e-com/src/views/ProductDetails.js
@@ -1,11 +1,27 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import ProductCard from '../components/products/ProductCard';
+import { clearProduct, getProductById } from '../store/actions/productCatalogActions';
 
 const ProductDetails = () => {
+
+    const id = useParams().id
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getProductById(id))
+
+        return () => {
+            dispatch(clearProduct())
+        }
+    }, [dispatch, id])
+
+    const product = useSelector(state => state.product.product)
+
     return (
         <div>
-            ProductDetails
+            { product && <ProductCard product={product} details={true} /> } //lektion 8 del1 2:27
         </div>
     )
 }
