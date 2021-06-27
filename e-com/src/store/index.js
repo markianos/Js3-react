7fc53fd155a33d1e2bf6db0e1fff1fd30a6a3504
Alexander Markianose <markianos.alexander@gmail.com>
2021-06-27 15:19:14 +0200

    store

---
+++ e-com/src/store/index.js
@@ -0,0 +1,11 @@
+import { creatStore, applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+export const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
